1b41aaa9c5558947b86dafb5d10315a6981fd0d6
Dhiraj Mahadik <dhirajmahadik9221@gmail.com>
2023-10-19 00:03:17 +0530

    NA

--- middlewares/sendMail.js
+++ middlewares/sendMail.js
@@ -1,6 +1,6 @@
 import nodemailer from 'nodemailer'
 
-const sendMail = (name, message, email, phone) => {
+const sendMail = (name, message, email) => {
 
     const transporter = nodemailer.createTransport({
         host: process.env.SMTP_HOST,
